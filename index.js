d12abb706486e26ff6213be8464ebda0c6425f12
Mayank Kumar Sharma <mayankece155@gmail.com>
2023-09-17 22:40:22 +0530

    insert many in db

--- index.js
+++ index.js
@@ -7,22 +7,52 @@ const databaseName = "task-manager";
 
 MongoClient.connect(connectionURL, {}, (error, client) => {
   if (error) {
-    return console.log("Unable to connect to database!");
+    return console.log('Unable to connect to database!')
   }
 
-  const db = client.db(databaseName);
+  const db = client.db(databaseName)
 
-  db.collection("users").insertOne(
-    {
-      name: "Andrew",
-      age: 27,
-    },
+  // db.collection('users').insertMany(
+  //   [
+  //     {
+  //       name: 'Jen',
+  //       age: 28,
+  //     },
+  //     {
+  //       name: 'Gunther',
+  //       age: 27,
+  //     },
+  //   ],
+  //   (error, result) => {
+  //     if (error) {
+  //       return console.log('Unable to insert documents!')
+  //     }
+
+  //     console.log(result.ops)
+  //   }
+  // )
+  db.collection('tasks').insertMany(
+    [
+      {
+        description: 'Clean the house',
+        completed: true,
+      },
+      {
+        description: 'Renew inspection',
+        completed: false,
+      },
+      {
+        description: 'Pot plants',
+        completed: false,
+      },
+    ],
     (error, result) => {
       if (error) {
-        return console.log("Unable to insert user");
+        return console.log('Unable to insert tasks!')
       }
 
-      console.log(result.ops);
-    },
-  );
+      console.log(result.ops)
+    }
+  )
+
 });
