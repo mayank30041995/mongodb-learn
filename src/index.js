900c7e8067e69a1106d9ff01d0b0b4baf90b4941
Mayank Kumar Sharma <mayankece155@gmail.com>
2023-09-23 23:21:13 +0530

    Separate routes

--- src/index.js
+++ src/index.js
@@ -2,24 +2,13 @@ const express = require('express')
 require('./db/mongoose')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const User = require('./models/user')
+
 const app = express()
 const port = process.env.PORT || 3000
-const router = new express.Router()
 
 app.use(express.json())
-app.use(router)
-// app.use(userRouter)
-// app.use(taskRouter)
-
-router.get('/users', async (req, res) => {
-  try {
-    const users = await User.find({})
-    res.send(users)
-  } catch (e) {
-    res.status(500).send()
-  }
-})
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
