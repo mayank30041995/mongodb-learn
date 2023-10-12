d4ad46a1bed191946b272c41929bd00de679d1e5
Mayank Kumar Sharma <mayankece155@gmail.com>
2023-10-12 15:00:00 +0530

    Multer Upload File and images

--- src/index.js
+++ src/index.js
@@ -6,17 +6,30 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
+const multer = require('multer')
+const upload = multer({
+  dest: 'images',
+  limits: {
+    fileSize: 1000000,
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error('Please upload a Word document'))
+    }
 
-// app.use((req, res, next) => {
-//     res.status(503).send('Site is currently down. Check back soon!')
-// })
+    cb(undefined, true)
+  },
+})
+app.post(
+  '/upload',
+  upload.single('upload'),
+  (req, res) => {
+    res.send()
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+  }
+)
 
 app.use(express.json())
 app.use(userRouter)
@@ -25,18 +38,3 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-const main = async () => {
-  const task = await Task.findById('651ea9731ea5e45a6044bb68')
-  await task.populate('owner')
-  console.log(task.owner)
-
-  // const user = await User.findById('6511c226611ea999f88e2700')
-  // await user.populate('tasks').execPopulate()
-  // console.log(user.tasks)
-}
-
-main()
